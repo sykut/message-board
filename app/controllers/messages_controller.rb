501263de68fef97d44d2bd6524e9034cf42238fc
saya.sato <sibuwaso@usako.net>
2018-06-18 07:59:44 +0000

    DAY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    
+
     if @message.save
       flash[:success] = 'Message が正常に投稿されました'
       redirect_to @message
@@ -24,12 +25,9 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -40,19 +38,19 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
 
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
-  
-  # Strong Parameter
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
-
 end
-
