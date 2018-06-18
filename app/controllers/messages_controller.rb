c3e1c72ff593052e1e09e8bd0931936bea377e9d
saya.sato <sibuwaso@usako.net>
2018-06-18 07:40:05 +0000

    CRUD messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,29 +29,29 @@ class MessagesController < ApplicationController
 
   def update
     @message = Message.find(params[:id])
-    
+
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
-      reading_to @message
+      redirect_to @message
     else
       flash.now[:danger] = 'Message は更新されませんでした'
-      reader :edit
+      render :edit
     end
   end
 
   def destroy
     @message = Message.find(params[:id])
     @message.destroy
-    
+
     flash[:success] = 'Message は正常に削除されました'
-    redirect_to message_url
+    redirect_to messages_url
   end
   
   private
   
   # Strong Parameter
   def message_params
-    @params.require(:message).permit(content)
+    params.require(:message).permit(:content)
   end
 
 end
